add6612f150706c049b772051c15d2fa9a259636
June <J.ryu.jieun@gmail.com>
2025-11-29 01:51:51 +0900

    Add project 'morethan-log' to site configuration

--- site.config.js
+++ site.config.js
@@ -10,7 +10,12 @@ const CONFIG = {
     github: "",
     instagram: "",
   },
-  projects: [],
+  projects: [
+    {
+      name: `morethan-log`,
+      href: "https://github.com/RYUJ1EUN/morethan-log",
+    },
+  ],
   // blog setting (required)
   blog: {
     title: "June-Log",
