ce3324512dc41b04ebd9be4fe1b911443eeec609
June <J.ryu.jieun@gmail.com>
2025-11-29 00:31:35 +0900

    Update profile and blog settings in site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "June",
     image: "/whale.svg", // "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Ph.D. student", // frontend developer",
+    bio: "I wish for a life full of books, exhibitions and jazz.",
+    email: "j.ryu.jieun@gmail.com",
+    linkedin: "",
+    github: "",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+    //  name: `morethan-log`,
+    //  href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "June-Log",
+    description: "Welcome to June's blog.",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
