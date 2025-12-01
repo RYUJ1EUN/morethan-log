dc29684c754458a90229950146eab952df778d50
June <J.ryu.jieun@gmail.com>
2025-12-01 12:41:18 +0900

    Update ScrollProgressBar background color
    Changed the background color of the ScrollProgressBar.

--- src/components/ScrollProgressBar.tsx
+++ src/components/ScrollProgressBar.tsx
@@ -29,7 +29,7 @@ const ProgressBar = styled.div`
   top: 0;
   left: 0;
   height: 6px; // 바 두께 (원하는 대로 조절 가능)
-  background: linear-gradient(to right, #6EE7B7, #3B82F6); // 색상 (그라데이션)
+  background: #c390d4; // linear-gradient(to right, #6EE7B7, #3B82F6); // 색상 (그라데이션)
   z-index: 9999; // 다른 요소보다 위에 뜨게 설정
   transition: width 0.1s ease-out; // 부드럽게 움직이도록 설정
   border-radius: 0 4px 4px 0;
