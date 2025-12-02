11195e5bce77bab2281be0897077c057af1ca700
June <J.ryu.jieun@gmail.com>
2025-12-02 13:38:13 +0900

    Implement right-click context menu prevention
    Added useEffect to prevent right-click context menu.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,26 @@ import { AppPropsWithLayout } from "../types"
 import { Hydrate, QueryClientProvider } from "@tanstack/react-query"
 import { RootLayout } from "src/layouts"
 import { queryClient } from "src/libs/react-query"
+// [추가 1] 리액트의 useEffect 기능을 가져옵니다.
+import { useEffect } from "react"
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
+
+  // [추가 2] 우클릭 방지 코드를 여기에 넣습니다.
+  useEffect(() => {
+    const handleContextmenu = (e: MouseEvent) => {
+      e.preventDefault()
+    }
+    document.addEventListener("contextmenu", handleContextmenu)
+    
+    return () => {
+      document.removeEventListener("contextmenu", handleContextmenu)
+    }
+  }, [])
+
+
+
 
   return (
     <QueryClientProvider client={queryClient}>
