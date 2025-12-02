b23d80cdac03bd1ad54d86602e918dcbf9b0fb92
June <J.ryu.jieun@gmail.com>
2025-12-02 22:31:36 +0900

    Remove user selection and drag restrictions
    Removed CSS and JavaScript restrictions on user selection and drag events.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,31 +36,8 @@ class MyDocument extends Document {
               />
             </>
           )}
-          {/* [강력 차단 1] CSS로 드래그/선택 원천 봉쇄 */}
-          <style>{`
-            html, body, #__next {
-              -webkit-user-select: none !important;
-              -moz-user-select: none !important;
-              -ms-user-select: none !important;
-              user-select: none !important;
-              -webkit-user-drag: none !important;
-            }
-            /* 입력창은 풀어줌 */
-            input, textarea {
-              -webkit-user-select: text !important;
-              -moz-user-select: text !important;
-              -ms-user-select: text !important;
-              user-select: text !important;
-            }
-          `}</style>
         </Head>
         <body>
-          {/* [강력 차단 2] 자바스크립트로 우클릭/드래그 이벤트 삭제 */}
-          onContextMenu={(e) => { e.preventDefault(); return false; }}
-          onDragStart={(e) => { e.preventDefault(); return false; }}
-          onSelectStart={(e) => { e.preventDefault(); return false; }}
-          style={{ userSelect: "none", WebkitUserSelect: "none" }} // 인라인 스타일로 한 번 더 잠금
-        >
           <Main />
           <NextScript />
         </body>
